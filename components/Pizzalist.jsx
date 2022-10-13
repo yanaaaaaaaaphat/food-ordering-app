5b40dcf43c15bb0fddb3e0e66853542670f309f2
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-10-13 13:44:43 +0700

    feat: backend development initiated

--- components/Pizzalist.jsx
+++ components/Pizzalist.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Pizzalist.module.css";
 import PizzaCard from "./PizzaCard";
 
-const PizzaList = () => {
+const PizzaList = ({pizzaList}) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
@@ -11,14 +11,10 @@ const PizzaList = () => {
         sit amet, consectetur adipscing elit.
       </p>
       <div className={styles.wrapper}>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
-        <PizzaCard/>
+        {Array.from(pizzaList).map((pizza) => (
+           <PizzaCard key={pizza._id} pizza={pizza}/>
+        ))}
+        
       </div>
     </div>
   );
